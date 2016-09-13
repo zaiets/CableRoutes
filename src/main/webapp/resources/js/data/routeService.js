a2ec217ed5e33723b4d13dbc76c25424ed1d60e7
Levsha <levsha.kh@gmail.com>
2016-09-13 16:08:16 +0300

    refactoring

--- src/main/webapp/resources/js/data/routeService.js
+++ src/main/webapp/resources/js/data/routeService.js
@@ -7,6 +7,18 @@ app.service('routeService', function ($http) {
         var rejected = [];
         for (var key in newEntities) {
             var current = newEntities[key];
+            if (current.routeTypeMarker) {
+                console.log('define route type');
+                current.routeType = $http.get('/routeType/' + current.routeTypeMarker);
+            }
+            if (current.joinPointFirstKks) {
+                console.log('define joinPoint #1');
+                current.joinPointFirst = $http.get('/joinPoint/' + current.joinPointFirstKks);
+            }
+            if (current.joinPointSecondKks) {
+                console.log('define joinPoint #2');
+                current.joinPointSecond = $http.get('/joinPoint/' + current.joinPointSecondKks);
+            }
             $http.put(uploadUrl, current)
                 .success(function () {
                     console.log('On data pushed:' + current + ' success');
