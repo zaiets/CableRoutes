0f70ab4a622169b306bbcaf08bfd8a43df04eba3
Levsha <levsha.kh@gmail.com>
2016-09-17 08:19:17 +0300

    fixed issues

--- src/main/webapp/resources/js/data/functionalityService.js
+++ src/main/webapp/resources/js/data/functionalityService.js
@@ -1,7 +1,7 @@
 app.service('functionalityService', function ($http) {
     this.defineEquipmentsWithNewPoints = function(result) {
         console.log('defineEquipmentsWithNewPoints works...');
-        $http.put('/define/pointsbyequips')
+        $http.put('/functionality/define/pointsbyequips')
             .then(function (response) {
                 console.log('server responded status ' + response.status);
                 if (response.status == 200) result(response.data);
